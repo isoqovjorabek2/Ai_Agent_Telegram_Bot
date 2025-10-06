72f4f33120942fa6ffa24440b2156d46f6f42eec
isoqovjorabek2 <isoqovjorabek774@gmail.com>
2025-10-06 12:16:33 +0500

    Second comment

--- webapp/env.js
+++ webapp/env.js
@@ -1,3 +1,3 @@
 // This file is auto-generated during build
 // Environment variables are injected here
-window.ENV_BACKEND_URL = "https://example-api.com";
+window.ENV_BACKEND_URL = "https://ai-agent-service-uz-lhhv.onrender.com";
